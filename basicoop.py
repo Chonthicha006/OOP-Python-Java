be8ceb6db4739bc6056923e2cfef968cf0fa028d
Chonthicha Phuangfueang <126902958+Chonthicha006@users.noreply.github.com>
2023-07-16 18:03:42 +0700

    .

--- basicoop.py
+++ basicoop.py
@@ -1,7 +1,11 @@
 #ชื่อ , เงินเดือน 
 
 class Employee:#การสร้างclass
-    pass
-
-#การเรียกใช้งานclass โดยการสร้างวัตถุ
+    #สร้างMethod
+     def detail(self): #ถ้าเป็นการเขียนโปรเเกรมทั่วไปเรียกว่า  ฟังก์ชัน ถ้าเขียนโปรเเกรมเชิงวัตถุจะเรียกว่า Method #ใส่self เพื่อให้รู้ว่าคือ method
+         print("เรียกใช้งาน Mrthod ใน Class Employee")
+#การเรียกใช้งานclass โดยการสร้างObject
 emp1 = Employee()
+emp1.detail()
+#กำหนดชื่อattribute(คุณลักษณะ)
+#Method/Behavier
