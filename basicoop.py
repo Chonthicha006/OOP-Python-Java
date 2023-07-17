065ab0207ce048086321c9d8e02cae279af3f98c
Chonthicha Phuangfueang <126902958+Chonthicha006@users.noreply.github.com>
2023-07-17 15:01:44 +0700

    Booth's Al.

--- basicoop.py
+++ basicoop.py
@@ -3,7 +3,8 @@
 class Employee:#การสร้างclass
     #สร้างMethod
      def detail(self):                             #ถ้าเป็นการเขียนโปรเเกรมทั่วไปเรียกว่า  ฟังก์ชัน ถ้าเขียนโปรเเกรมเชิงวัตถุจะเรียกว่า Method #ใส่self เพื่อให้รู้ว่าคือ Method/Behavier 
-         self.name="A"                             #กำหนด Attribute ผ่าน keyword => self nameคือชื่อ Attribute
+       #กำหนด Attribute ผ่าน keyword => self       nameคือ ชื่อAttribute
+         self.name="A"                             
          self.salary=20000
          print("Name={}".format(self.name))
          print("Salary={}".format(self.salary))      #เรียกใช้งาน Method 
