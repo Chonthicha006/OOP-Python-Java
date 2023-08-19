0a0f925cd7fde6b61285e528bb7f06e301d364a7
Chonthicha Phuangfueang <126902958+Chonthicha006@users.noreply.github.com>
2023-08-19 10:34:43 +0700

    add new

--- lab1.java
+++ lab1.java
@@ -4,24 +4,24 @@
 *
 */
 public class BicycleDemo {
-    public static void main(String[] args) {
-    // Create two different Bicycle objects
-    Bicycle bike1 = new Bicycle();
-    Bicycle bike2 = new Bicycle();
-    System.out.println(bike1);
-    // Invoke methods on those objects
-    bike1.changeCadence(50);
-    bike1.changeSpeed(10);
-    bike1.changeGear(2);
-    bike1.printGear();
-    System.out.println(bike1);
-    bike2.changeCadence(50);
-    bike2.changeSpeed(10);
-    bike2.changeGear(2);
-    bike2.changeCadence(40);
-    bike2.changeSpeed(-10);
-    bike2.changeGear(3);
-    bike2.printGear();
-    System.out.println(bike1);
-    }
-    }
+public static void main(String[] args) {
+// Create two different Bicycle objects
+Bicycle bike1 = new Bicycle();
+Bicycle bike2 = new Bicycle();
+System.out.println(bike1);
+// Invoke methods on those objects
+bike1.changeCadence(50);
+bike1.changeSpeed(10);
+bike1.changeGear(2);
+bike1.printGear();
+System.out.println(bike1);
+bike2.changeCadence(50);
+bike2.changeSpeed(10);
+bike2.changeGear(2);
+bike2.changeCadence(40);
+bike2.changeSpeed(-10);
+bike2.changeGear(3);
+bike2.printGear();
+System.out.println(bike1);
+}
+}
