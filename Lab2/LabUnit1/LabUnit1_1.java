6fb442ef15bd1fcd2d3d326ed163cb29c7252fd0
Chonthicha Phuangfueang <126902958+Chonthicha006@users.noreply.github.com>
2023-09-08 12:06:57 +0700

    add newHW

---
+++ Lab2/LabUnit1/LabUnit1_1.java
@@ -0,0 +1,12 @@
+/*Write a program to display message
+Example : display a sentence: "Welcome to OOP Programming"*/
+
+public class LabUnit1_1 
+{
+    public static void main (String[] args)
+    {
+        String Message = "Sawasdee";
+        System.out.println("Message:" +Message);
+    }
+    
+}
